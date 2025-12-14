7a96ab973f52a2fc2bebc636b8c57c734525629a
injamul007 <injamul007hoque@gmail.com>
2025-12-14 11:02:20 +0600

    update and add the theme toggle mode in big spinner white component

--- src/components/Shared/BigLoadSpinnerWhite/BigLoadSpinnerWhite.jsx
+++ src/components/Shared/BigLoadSpinnerWhite/BigLoadSpinnerWhite.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const BigLoadSpinnerWhite = () => {
+  const [theme] = useState(
+    () =>
+      localStorage.getItem("theme") ||
+      (window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? "dark"
+        : "light")
+  );
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
   return (
-    <div className="flex justify-center items-center min-h-screen bg-white">
+    <div
+      className={
+        theme === "dark"
+          ? "min-h-screen bg-[#1d232a] text-gray-100 flex items-center justify-center flex-col"
+          : "min-h-screen bg-white text-gray-900 flex items-center justify-center flex-col"
+      }
+    >
       <img
         className="w-20 animate-spin"
         src={"https://i.ibb.co.com/4wCJZ08Y/react-JS-Logo.png"}
