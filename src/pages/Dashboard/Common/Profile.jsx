68747d3379481af5aa0c4a4b2bfba67f398c827d
injamul007 <injamul007hoque@gmail.com>
2025-12-21 19:36:59 +0600

    update the profile with new UI

--- src/pages/Dashboard/Common/Profile.jsx
+++ src/pages/Dashboard/Common/Profile.jsx
@@ -1,107 +1,113 @@
 import MyContainer from "../../../components/Shared/MyContainer/MyContainer";
 import useAuth from "../../../hooks/useAuth";
-import Swal from "sweetalert2";
+import useRole from "../../../hooks/useRole";
+import SpinnerForDashboardRoute from "../../../components/Shared/SpinnerForDashboardRoute/SpinnerForDashboardRoute";
 import { RiLogoutBoxRLine } from "react-icons/ri";
+import Swal from "sweetalert2";
 import profileCoverImg from "../../../assets/user_cover_img.png";
 import managerCoverImg from "../../../assets/manager_cover.jpg";
 import adminCoverImg from "../../../assets/admin_cover.jpg";
-import useRole from "../../../hooks/useRole";
-import SpinnerForDashboardRoute from "../../../components/Shared/SpinnerForDashboardRoute/SpinnerForDashboardRoute";
 
 const Profile = () => {
   const { user, logOutFunc, setUser } = useAuth();
-  const {role, isRoleLoading} = useRole();
+  const { role, isRoleLoading } = useRole();
 
   const handleLogout = async () => {
-    try {
-      await logOutFunc();
-      Swal.fire({
-        position: "top-end",
-        icon: "success",
-        title: "LogOut Successful",
-        showConfirmButton: false,
-        timer: 1500,
-        customClass: {
-          popup: "small-swal-popup",
-        },
-      });
-      setUser(null);
-    } catch (err) {
-      console.error(err);
-    }
+    await logOutFunc();
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      title: "LogOut Successful",
+      showConfirmButton: false,
+      timer: 2000,
+      customClass: {
+        popup: "small-swal-popup",
+      },
+    });
+    setUser(null);
   };
 
-  if(isRoleLoading) return <SpinnerForDashboardRoute></SpinnerForDashboardRoute>
+  if (isRoleLoading) return <SpinnerForDashboardRoute />;
+
   return (
-    <div className="py-12 px-18">
-      <MyContainer>
-        <title>LoanLink || Profile</title>
-        <div className="flex justify-center items-center h-screen">
-          <div className=" dark:bg-[#2b3138] shadow-lg rounded-2xl max-w-2xl mx-auto">
-            <img
-              alt="cover photo"
-              src={role === 'admin' ? adminCoverImg : role === 'manager' ? managerCoverImg : profileCoverImg}
-              className="w-full mb-4 rounded-t-lg h-56"
-            />
-            <div className="flex flex-col items-center justify-center p-4 -mt-16">
-              <a href="#" className="relative block">
-                <img
-                  alt="profile"
-                  src={user?.photoURL}
-                  className="mx-auto object-cover rounded-full h-24 w-24  border-2 border-white "
-                />
-              </a>
+    <MyContainer>
+      <title>LoanLink || My Profile</title>
+      <div className="bg-base-200 dark:bg-[#2b3138] rounded-2xl shadow-md overflow-hidden mt-8 mb-8 md:mt-12 md:mb-12 lg:mt-22">
+        {/* Cover */}
+        <div className="relative md:h-12 lg:h-30">
+          <img
+            alt="cover photo"
+            src={
+              role === "admin"
+                ? adminCoverImg
+                : role === "manager"
+                ? managerCoverImg
+                : profileCoverImg
+            }
+            className=" mb-4 rounded-t-lg object-cover"
+          />
+        </div>
 
-              <p className="p-2 px-4 text-xs text-white bg-primary rounded-full">
+        {/* Profile Info */}
+        <div className="relative px-6 pb-6 md:mt-2 mt-0">
+          <img
+            src={user?.photoURL}
+            className="w-24 h-24 rounded-full border-4 border-white absolute -top-12"
+          />
+
+          <div className="pt-14 flex flex-col md:flex-row md:justify-between pb-6">
+            <div>
+              <h2 className="text-2xl text-gray-600 md:text-gray-200 dark:text-white font-bold">
+                {user?.displayName}
+              </h2>
+              <p className="text-gray-600 md:text-gray-200 dark:text-white text-sm">
+                {role === "admin"
+                  ? "Admin of LoanLink Management System"
+                  : role === "manager"
+                  ? "Manager of LoanLink Management System"
+                  : "I am a Borrower"}
+              </p>
+              <p className="text-gray-600 md:text-gray-200 dark:text-white text-sm">
+                Email: {user?.email}
+              </p>
+            </div>
+
+            <div className="mt-2 md:mt-0 lg:mb-0 md:mb-0 mb-2">
+              <span className="text-sm font-semibold text-gray-600 md:text-gray-200 dark:text-white">
+                Current role
+              </span>
+              <div className="mt-1 px-4 py-1 bg-[#4DA3FF] rounded-full text-sm font-bold text-center text-black">
                 {role}
-              </p>
+              </div>
+            </div>
+          </div>
 
-              <p className="mt-2 lg:text-xl inline-flex flex-wrap font-medium text-gray-600 dark:text-white">
-                User Id: {user?.uid}
-              </p>
-              <div className="w-full p-2 mt-4 rounded-lg">
-                <div className="grid lg:grid-cols-2 text-sm text-gray-600 ">
-                  <p className="flex flex-col mb-4">
-                    <span className="text-gray-600 dark:text-white">Name</span>
-                    <span className="font-bold text-gray-600 dark:text-white">
-                      {user?.displayName}
-                    </span>
-                  </p>
-                  <p className="flex flex-col mb-4">
-                    <span className="text-gray-600 dark:text-white">Email</span>
-                    <span className="font-bold text-gray-600 dark:text-white">
-                      {user?.email}
-                    </span>
-                  </p>
-                </div>
-                <div>
-                  <div className="grid lg:grid-cols-3 gap-2">
-                    <div>
-                      <button className="bg-[#4DA3FF] font-bold  px-10 py-2 rounded-lg text-white cursor-pointer hover:bg-primary dark:hover:bg-[#FFB703] w-full block mb-1 duration-300">
-                        Update Profile
-                      </button>
-                    </div>
-                    <div>
-                      <button className="bg-[#4DA3FF] px-7 py-2 rounded-lg text-white cursor-pointer hover:bg-primary font-bold dark:hover:bg-[#FFB703] w-full block mb-1 duration-300">
-                        Change Password
-                      </button>
-                    </div>
-                    <div>
-                      <button
-                        onClick={handleLogout}
-                        className="inline-flex justify-center items-center gap-2 bg-[#4DA3FF] hover:bg-primary cursor-pointer px-7 py-2 w-full rounded-lg text-white duration-300 font-bold dark:hover:bg-[#FFB703]"
-                      >
-                        <RiLogoutBoxRLine size={20} /> Logout
-                      </button>
-                    </div>
-                  </div>
-                </div>
+          {/* Buttons */}
+          <div>
+            <div className="grid lg:grid-cols-3 gap-2">
+              <div>
+                <button className="bg-[#4DA3FF] font-bold  px-10 py-2 rounded-lg text-white cursor-pointer hover:bg-primary dark:hover:bg-[#FFB703] w-full block mb-1 duration-300">
+                  Update Profile
+                </button>
+              </div>
+              <div>
+                <button className="bg-[#4DA3FF] px-7 py-2 rounded-lg text-white cursor-pointer hover:bg-primary font-bold dark:hover:bg-[#FFB703] w-full block mb-1 duration-300">
+                  Change Password
+                </button>
+              </div>
+              <div>
+                <button
+                  onClick={handleLogout}
+                  className="inline-flex justify-center items-center gap-2 bg-[#4DA3FF] hover:bg-primary cursor-pointer px-7 py-2 w-full rounded-lg text-white duration-300 font-bold dark:hover:bg-[#FFB703]"
+                >
+                  <RiLogoutBoxRLine size={20} /> Logout
+                </button>
               </div>
             </div>
           </div>
         </div>
-      </MyContainer>
-    </div>
+      </div>
+    </MyContainer>
   );
 };
 
